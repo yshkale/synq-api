974a49e6d68fc5b753d49447c13d8bd2a93c5419
Yash <ykale@hootboard.com>
2025-02-23 16:57:50 +0530

    feat: :sparkles: add database connection

--- src/server.js
+++ src/server.js
@@ -3,8 +3,11 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const connectDB = require("../config/dB");
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use(cors());
