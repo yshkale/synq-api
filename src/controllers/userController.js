10dd79ecafaf7808f226e1a9d835c9ffd7c9f229
Yash <ykale@hootboard.com>
2025-03-01 12:50:10 +0530

    feat: :sparkles: return token when user signup

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -14,10 +14,17 @@ const createUser = async (req, res) => {
     }
 
     const user = await User.create({ name, email, password });
+
+    //generate jwt token
+    const token = sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
     res.status(201).json({
       id: user._id,
       name: user.name,
       email: user.email,
+      token,
     });
   } catch (err) {
     res.status(500).json({
