4d37596a78428fca845f88ca134cce4e077f0bf2
Yash <ykale@hootboard.com>
2025-04-14 19:19:11 +0530

    feat: :fire: remove the forgot reset password functionality

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,8 +3,6 @@ const {
   createUser,
   userLogin,
   getUserData,
-  forgotPassword,
-  resetPassword,
 } = require("../controllers/userController");
 
 const { protect } = require("../middlewares/authMiddleware");
@@ -13,9 +11,6 @@ const router = express.Router();
 
 router.post("/signup", createUser);
 router.post("/login", userLogin);
-
-router.post("/forgot-password", forgotPassword);
-router.patch("/reset-password/:token", resetPassword);
 
 router.get("/get-user-data", protect, getUserData);
 
